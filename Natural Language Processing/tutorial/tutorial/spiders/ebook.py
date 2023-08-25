0dec9bc81ff01c6fd611fb3b0eb7c94cea0ed9aa
AnushaNarayananP <ammu142981@gmail.com>
2023-08-25 20:53:12 +0530

    after correcting ,started to code

---
+++ Natural Language Processing/tutorial/tutorial/spiders/ebook.py
@@ -0,0 +1,11 @@
+import scrapy
+class EbookSpider(scrapy.Spider):
+    name='ebook'
+    start_urls = ['https://books.toscrape.com/']
+    def parse(self,response):
+        print('[parse]')
+        ebooks= response.css('article')
+        for ebook in ebooks:
+            title =ebook.css('a::text').get()
+            price =ebook.css('p')
+        

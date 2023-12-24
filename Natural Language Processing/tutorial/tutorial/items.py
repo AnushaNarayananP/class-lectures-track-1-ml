96d1d537759938e9e5a2efc204d00c3ae46b7140
AnushaNarayananP <ammu142981@gmail.com>
2023-12-24 22:07:27 +0530

    updated

--- Natural Language Processing/tutorial/tutorial/items.py
+++ Natural Language Processing/tutorial/tutorial/items.py
@@ -3,10 +3,16 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from  scrapy import Item,Field
+from itemloaders.processors import MapCompose ,TakeFirst
 
+def get_price(txt):
+    return float(txt.replace('£',''))
 
-class TutorialItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class TutorialItem(Item):
+    
+    title=Field()
+    price=Field(
+        input_processor =MapCompose(get_price),
+        output_processor = TakeFirst()
+    )
--- Natural Language Processing/tutorial/tutorial/spiders/ebook.py
+++ Natural Language Processing/tutorial/tutorial/spiders/ebook.py
@@ -1,10 +1,19 @@
 import scrapy
+from tutorial.items import TutorialItem
+from scrapy.loader import ItemLoader
+def get_price(txt):
+    return float(txt.replace('£',''))
 class EbookSpider(scrapy.Spider):
     name='ebook'
     start_urls = ['https://books.toscrape.com/']
     def parse(self,response):
-        print('[parse]')
-        print(response.xpath('//h3/a/@title').get())
+        ebooks=response.css('article.product_pod')
+        for book in books:
+            loader=ItemLoader(item=TutorialItem())
+
+            ebook_item[title]=ebook.css('h3 a').attrib('title')
+            ebook_item[price]=get_price(ebook.css('p.price_color::text').get())
+            yield ebook_item
         
 
         
